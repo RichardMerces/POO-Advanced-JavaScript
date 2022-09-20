93e5d81d564be50e868a1b79646731d9a1e93130
RichardMerces <richardmerces@outlook.com>
2022-09-20 16:15:38 -0300

    Acrescentando mais informações ao paciente

--- atividade3.ts
+++ atividade3.ts
@@ -7,9 +7,12 @@ class Paciente {
     emfermaria:string;
     nome: string;
     leito: number;
+    remedio: string;
+    qtd: string;
+    horario: string;
 
     mostrarInformacoes(){
-        console.log(`Emfermaria: ${this.emfermaria}\nNome: ${this.nome}\nLeito: ${this.leito}\n`)
+        console.log(`Emfermaria: ${this.emfermaria}\nNome: ${this.nome}\nLeito: ${this.leito}\nRemédio: ${this.remedio}\nQuantidade: ${this.qtd}\nHorário: ${this.horario}\n`)
     }
 }
 
@@ -17,6 +20,9 @@ const paci = new Paciente();
 paci.emfermaria = "02";
 paci.nome = "Maria";
 paci.leito = 10;
+paci.remedio = "Bezetacil";
+paci.qtd = "3ml";
+paci.horario = "15h40min";
 
 paci.mostrarInformacoes();
 
